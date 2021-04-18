0963cc6efcdbbd224003d5ac6e62ec4d43df5485
ashenoy20 <ashishshenoy9@gmail.com>
2021-04-18 00:47:00 -0400

    Some changes to donut layout

--- app/src/main/java/com/example/rucafe/DonutActivity.java
+++ app/src/main/java/com/example/rucafe/DonutActivity.java
@@ -10,5 +10,6 @@ public class DonutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donut);
+        setTitle("Order Donuts");
     }
 }
